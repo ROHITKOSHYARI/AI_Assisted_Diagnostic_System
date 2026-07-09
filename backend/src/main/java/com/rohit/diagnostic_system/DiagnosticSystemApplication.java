5861790565db183c6a87d533808b67dc4acb4471
rohitkoshyari <rohitkoshyari7244@gmail.com>
2026-07-09 23:03:09 +0530

    initial commit

---
+++ backend/src/main/java/com/rohit/diagnostic_system/DiagnosticSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.diagnostic_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rohit/diagnostic_system/DiagnosticSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.diagnostic_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
